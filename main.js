e4206840dfc137babdc53a005d2d3641aa5744bb
ltran1153 <s3963174@rmit.edu.vn>
2026-04-07 21:04:39 +1000

    update

--- main.js
+++ main.js
@@ -5,18 +5,6 @@ const stage = new Konva.Stage({
   height: 500,
 })
 
-const firstLayer = new Konva.Layer()
-
-const circle = new Konva.Circle({
-  x: 250,
-  y: 250,
-  radius: 50,
-  fill: "red",
-})
-
-firstLayer.add(circle)
-stage.add(firstLayer)
-
 //drawing feature
 //feature analysis
 //User goal: trying to draw a picture?
@@ -28,38 +16,15 @@ stage.add(firstLayer)
 //how does it interact with other features?
 //color, brush texture, eraser, upload image
 
-//keep track of when button is held
-let isDrawing = false
-let lastLine
+const adjectiveLayer = new konva.layer()
+stage.add(adjectiveLayer)
 
-//user presses mouse button
-function drawMouseDown() {
-  isDrawing = true
-  const pos = stage.getPointerPosition()
-  let newPoints = lastLine.points().concat(strings: [pos.x, pos.y]);
-  lastLine.points(newPoints);
-  lastLine = new Konva.line({
-    stroke: "red",
-    strokeWidth: 5,
-    lineCap: "round",
-    lineJoin: "round",
-    points: [pos.x, pos.y, pos.x, pos.y],
-  })
-  firstLayer.add(lastLine)
-}
-stage.on("mousedown", drawMouseDown)
-// user moves mouse
-function drawMouseMove() {
-  //don't run if not drawing
-  if (isDrawing === false) {
-    return
-  }
-  //if isDrawing is true
-}
-stage.on("mousemove", drawMouseMove)
-// user releases mouse
-function drawMouseUp() {
-  isDrawing = false
-}
-stage.on("mouseup", drawMouseUp)
-window.addEventListener( type: "mouseup", drawMouseUp)
+const adjectivesList = [
+  "Sublime",
+  "Quaint",
+  "Altruistic",
+  "Bamboozled",
+  "Preposterous",
+]
+const Xtranslation = Math.random() * stage.width()
+const Ytranslation = Math.random() * stage.height()
